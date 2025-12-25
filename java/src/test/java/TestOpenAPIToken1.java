fc221c00bc09b77714f96669be755244c3461b5c
airtom0816 <airtom_us@outlook.com>
2025-12-25 12:01:03 +0800

    Add files via upload

---
+++ java/src/test/java/TestOpenAPIToken1.java
@@ -0,0 +1,18 @@
+package com.example.openapi;
+import com.fasterxml.jackson.databind.ObjectMapper;
+public class TestOpenAPIToken1 {
+    public static void main(String[] args) {
+        String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhaXJ0b20iLCJpZCI6MTAsImFjY291bnQiOiLotbXno4oiLCJzdWIiOiLotbXno4oiLCJpYXQiOjE3NjQ1NzA2NDIsImV4cCI6MzMzMDA1NzA2NDJ9.cDnyVC_FiAy96p1yg22mTiEaLA_oHeVRBVMdjh_qlsA";
+        String BASE_URL = "http://10.0.0.132";
+        String tableName = "ge75c0b7e0e58eb21";
+        String URL = "/openapi/asset/datamodel/detail?tableName=" + tableName;       
+        try (OpenAPITokenClient client = new OpenAPITokenClient(BASE_URL, TOKEN)) {
+            Object result = client.get(URL);           
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResult = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+            System.out.println(jsonResult);           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
